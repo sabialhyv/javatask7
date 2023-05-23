d89038cd5c9a1765984740540717ba0bb89dcf94
Sabina Allahyarova <125457757+sabialhyv@users.noreply.github.com>
2023-05-23 20:43:52 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+            try {
+            int number1 = 10;
+            int number2 = 0;
+            int result = number1 / number2;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
     }
 }
